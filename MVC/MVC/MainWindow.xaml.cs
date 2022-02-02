8409ae0680994932938a869da18efecfab4f2902
StiffixStuff <94129618+StiffixStuff@users.noreply.github.com>
2022-02-02 11:01:41 +0300

    Add files via upload

---
+++ MVC/MVC/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorMVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double result;
+        double x1, x2;
+        string s;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tb = Result;
+            Model.tbCombo = ComboChange;
+            Model.tbx1 = ChangeX1;
+            Model.tbx2 = ChangeX2;
+            ComboChange.ItemsSource = Model.dataList;
+
+        }
+
+        private void controller_Click(object sender, RoutedEventArgs e)
+        {
+            if (s == "Сложение")
+            {
+                result = x1 + x2;
+            }
+            if (s == "Вычитание")
+            {
+                result = x1 - x2;
+            }
+            if (s == "Умножение")
+            {
+                result = x1 * x2;
+            }
+            if (s == "Деление")
+            {
+                result = x1 / x2;
+            }
+
+            Model.tb.Text = result.ToString();
+        }
+
+        private void ChangeX1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            x1 = Convert.ToDouble(Model.tbx1.Text);
+        }
+
+        private void ChangeX2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            x2 = Convert.ToDouble(Model.tbx2.Text);
+        }
+
+        private void ComboChange_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            s = Model.dataList[ComboChange.SelectedIndex];
+            Model.Combo = ComboChange.SelectedIndex;
+        }
+    }
+}
---
+++ MVC/MVC/Model.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace CalculatorMVC
+{
+    static class Model
+    {
+        public static List<string> dataList = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+        static double result;
+        public static TextBlock tb;
+        public static ComboBox tbCombo;
+        public static TextBox tbx1;
+        public static TextBox tbx2;
+
+        public static int Combo
+        {
+            set
+            {
+                tbCombo.Text = dataList[value].ToString();
+            }
+        }
+
+        public static double Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                tb.Text = result.ToString();
+            }
+        }
+    }
+}
